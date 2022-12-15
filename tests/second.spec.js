7a31a5ae0363a2cbd9c091cb418415aa478d3970
zagi <luqman.zagi@gmail.com>
2022-12-15 15:19:39 +0100

    Latihan pertama pake Playwright

---
+++ tests/second.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test');
+
+test('Second Test', async({page}) => {
+    await page.goto('https://www.programsBuzz.com/user/login')
+
+    //use id name
+    await page.locator('#edit-name').type('username')
+
+    //use xpath
+    await page.locator("//input[@id='edit-pass']").type('mypassword')
+
+    //click button
+    await page
+        .getByRole('button', {name: 'op'})
+        .click() 
+});
